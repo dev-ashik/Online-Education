37fdd4c43ab001168a9986c5dfb0931c2928d3b7
MD ASHIK MAHMUD <ashik.swfu@outlook.com>
2022-08-15 13:08:42 +0800

    our course section done

--- app.js
+++ app.js
@@ -3,36 +3,27 @@ const navbar = document.querySelector(".navbar");
 const header = document.querySelector("header");
 const section = document.querySelector("section");
 
-
-faBars.addEventListener("click", function() {
-    faBars.classList.toggle("fa-xmark");
-    // add className "fa-xmark" when click on fa-bars class
-    navbar.classList.toggle("nav-toggle");
-    // add className "nav-toggle" when click on fa-bars
-})
-
+faBars.addEventListener("click", function () {
+  faBars.classList.toggle("fa-xmark");
+  // add className "fa-xmark" when click on fa-bars class
+  navbar.classList.toggle("nav-toggle");
+  // add className "nav-toggle" when click on fa-bars
+});
 
 // remove class on scroll
-window.addEventListener('scroll', function() {
-    faBars.classList.remove("fa-xmark");
-    navbar.classList.remove("nav-toggle");
+window.addEventListener("scroll", function () {
+  faBars.classList.remove("fa-xmark");
+  navbar.classList.remove("nav-toggle");
 
-    if(window.scrollY > 30) {
-        header.classList.add("header-active");
-    }else {
-        header.classList.remove("header-active");
-    }
-
-    // const top = window.scrollTo({
-    //     top: 0,
-    //     left: 0
-    // });
-
-})
+  if (window.scrollY > 30) {
+    header.classList.add("header-active");
+  } else {
+    header.classList.remove("header-active");
+  }
+});
 
 // remove class on load
-window.addEventListener('load', function() {
-    faBars.classList.remove("fa-xmark");
-    navbar.classList.remove("nav-toggle");
-})
-
+window.addEventListener("load", function () {
+  faBars.classList.remove("fa-xmark");
+  navbar.classList.remove("nav-toggle");
+});
